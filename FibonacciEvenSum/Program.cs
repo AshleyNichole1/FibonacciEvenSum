1c8beab9672999f33498f629c87b238111cb7fca
AshleyNichole1 <ashleynicholelea@outlook.com>
2021-10-05 17:30:57 -0500

    Initial Commit and solution

---
+++ FibonacciEvenSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long currentV = 2;
+            long previousV = 1;
+            long nextV = 0;
+            long evensum = 2;
+
+            while (nextV < 4000000)
+            {
+                nextV = currentV + previousV;
+                previousV = currentV;
+                currentV = nextV;
+
+                if (currentV % 2 == 0)
+                {
+                    evensum += currentV;
+                }
+
+            }
+
+            Console.WriteLine(evensum);
+
+
+        }
+    }
+}
